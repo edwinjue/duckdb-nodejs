9045f3ff9e6dd2f2f28e51abe7e1fb1bc7f18469
Edwin J <hungrylulu8@gmail.com>
2023-06-01 23:18:24 -0700

    added drop table if exists to avoid getting error for recreating an existing table. added db.close()

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import duckdb from 'duckdb'
 const db = new duckdb.Database('./db/master.duckdb')
 
 // Create `requests` table
+db.exec("DROP TABLE IF EXISTS requests")
 db.exec("CREATE TABLE requests(SRNumber VARCHAR(255),CreatedDate TIMESTAMP,UpdatedDate TIMESTAMP,ActionTaken VARCHAR(255),Owner VARCHAR(255),RequestType VARCHAR(255),Status VARCHAR(255),RequestSource VARCHAR(255),CreatedByUserOrganization VARCHAR(255),MobileOS VARCHAR(255),Anonymous VARCHAR(255),AssignTo VARCHAR(255),ServiceDate TIMESTAMP,ClosedDate TIMESTAMP,AddressVerified VARCHAR(255),ApproximateAddress VARCHAR(255),Address VARCHAR(255),HouseNumber INTEGER,Direction VARCHAR(255),StreetName VARCHAR(255),Suffix VARCHAR(255),ZipCode INTEGER,Latitude DECIMAL(8,6),Longitude DECIMAL(9,6),Location VARCHAR(255),TBMPage INTEGER,TBMColumn VARCHAR(255),TBMRow INTEGER,APC VARCHAR(255),CD INTEGER,CDMember VARCHAR(255),NC INTEGER,NCName VARCHAR(255),PolicePrecinct VARCHAR(255))")
 
 // Seed it
@@ -23,3 +24,5 @@ db.all('SELECT * FROM requests limit 1', function(err, res) {
     console.error("Error occured:",e)
   }
 });
+
+db.close()
